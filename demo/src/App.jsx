3d3e6f65efafab69b5b7991437b8cddc75db4a5a
Jayasurya-SM <jayasurya365@outlook.com>
2023-09-30 20:17:28 +0530

    basic react created

---
+++ demo/src/App.jsx
@@ -0,0 +1,39 @@
+
+import './App.css';
+
+let userdata ={
+  name : "Surya",
+  imgurl:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Desktop_computer_clipart_-_Yellow_theme.svg/2560px-Desktop_computer_clipart_-_Yellow_theme.svg.png",
+  age: 0
+}
+
+function Truevalue(){
+  return(<div>
+    <h1>Welcome:{userdata.name}</h1>
+    <img src ={userdata.imgurl} alt={'photo of the user: '+ userdata.name} width={"200px"}/>
+    <p>The age of the user is {userdata.age}</p>
+  </div>
+
+  );
+}
+function Falsyvalue(){
+  return(
+    <div>
+      <h1>Welcome :user</h1>
+      <img src=" https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png" alt='' width={"150px"}/>
+      <p>The age is Unknown</p>
+    </div>
+
+  );
+}
+
+function App() {
+  return (
+    <div>
+      {userdata.name&&userdata.imgurl&&userdata.age ? <Truevalue/>: <Falsyvalue/>}
+    </div>
+    
+  );
+}
+
+export default App;
